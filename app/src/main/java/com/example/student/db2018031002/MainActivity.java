78b7b21cded5129cc693abae11033beb276b43fd
Shaojung <shaojung@gmail.com>
2018-03-10 14:21:08 +0800

    Settings Activity

--- app/src/main/java/com/example/student/db2018031002/MainActivity.java
+++ app/src/main/java/com/example/student/db2018031002/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.student.db2018031002;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,5 +33,12 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent it = new Intent(MainActivity.this, SettingsActivity.class);
         startActivity(it);
+    }
+    public void clickNick(View v)
+    {
+        TextView tv = findViewById(R.id.textView);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String str = sp.getString("sp_nickname", "Nickname");
+        tv.setText(str);
     }
 }
